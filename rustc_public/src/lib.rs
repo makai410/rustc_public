fdcadf9ee8a2e9717bf6ae0ddf4232600314df76
Makai <m4kai410@gmail.com>
2025-10-22 12:00:35 +0800

    Establish Infrastructure (#105)
    * make rustc_public a `crates.io` crate
    
    * establish infrastructrure
    
    * move `tests/` to `rustc_public/`
    
    s
    
    * conditional compilation of rustc crates
    
    * remove redundant content in rustfmt.toml
    
    * remove rust-toolchain.toml of rustc_public
    
    * add `Cargo.lock`

--- rustc_public/src/lib.rs
+++ rustc_public/src/lib.rs
@@ -43,8 +43,26 @@
 //! For more information, see <https://github.com/rust-lang/rustc_public>.
 
 #![allow(rustc::usage_of_ty_tykind)]
+#![cfg_attr(not(feature = "rustc-build"), feature(rustc_private))]
 #![doc(test(attr(allow(unused_variables), deny(warnings), allow(internal_features))))]
 #![feature(sized_hierarchy)]
+
+#[cfg(not(feature = "rustc-build"))]
+macro_rules! rustc_crates {
+    () => {
+        extern crate rustc_abi;
+        extern crate rustc_driver;
+        extern crate rustc_hir;
+        extern crate rustc_middle;
+        extern crate rustc_public_bridge;
+        extern crate rustc_session;
+        extern crate rustc_span;
+        extern crate rustc_target;
+    };
+}
+
+#[cfg(not(feature = "rustc-build"))]
+rustc_crates!();
 
 use std::fmt::Debug;
 use std::marker::PhantomData;
